e24d203116a84417395d70a206703c6663da1e7b
Andrew <acekane1@gmail.com>
2018-05-11 02:24:23 -0700

    Use separate classes

--- lib/blind_index/extensions.rb
+++ lib/blind_index/extensions.rb
@@ -1,48 +1,50 @@
 module BlindIndex
   module Extensions
+    # ActiveRecord 5.0+
     module TableMetadata
-      if ActiveRecord::VERSION::MAJOR >= 5
-        def resolve_column_aliases(hash)
-          new_hash = super
-          if has_blind_indexes?
-            hash.each do |key, _|
-              if (bi = klass.blind_indexes[key])
-                new_hash[bi[:bidx_attribute]] = BlindIndex.generate_bidx(new_hash.delete(key), bi)
-              end
+      def resolve_column_aliases(hash)
+        new_hash = super
+        if has_blind_indexes?
+          hash.each do |key, _|
+            if (bi = klass.blind_indexes[key])
+              new_hash[bi[:bidx_attribute]] = BlindIndex.generate_bidx(new_hash.delete(key), bi)
             end
           end
-          new_hash
         end
+        new_hash
+      end
 
-        # memoize for performance
-        def has_blind_indexes?
-          unless defined?(@has_blind_indexes)
-            @has_blind_indexes = klass.respond_to?(:blind_indexes)
-          end
-          @has_blind_indexes
+      # memoize for performance
+      def has_blind_indexes?
+        unless defined?(@has_blind_indexes)
+          @has_blind_indexes = klass.respond_to?(:blind_indexes)
         end
-      else
-        def resolve_column_aliases(klass, hash)
-          new_hash = super
-          if has_blind_indexes?(klass)
-            hash.each do |key, _|
-              if (bi = klass.blind_indexes[key])
-                new_hash[bi[:bidx_attribute]] = BlindIndex.generate_bidx(new_hash.delete(key), bi)
-              end
+        @has_blind_indexes
+      end
+    end
+
+    # ActiveRecord 4.2
+    module PredicateBuilder
+      def resolve_column_aliases(klass, hash)
+        new_hash = super
+        if has_blind_indexes?(klass)
+          hash.each do |key, _|
+            if (bi = klass.blind_indexes[key])
+              new_hash[bi[:bidx_attribute]] = BlindIndex.generate_bidx(new_hash.delete(key), bi)
             end
           end
-          new_hash
         end
+        new_hash
+      end
 
-        @@blind_index_cache = {}
+      @@blind_index_cache = {}
 
-        # memoize for performance
-        def has_blind_indexes?(klass)
-          if @@blind_index_cache[klass].nil?
-            @@blind_index_cache[klass] = klass.respond_to?(:blind_indexes)
-          end
-          @@blind_index_cache[klass]
+      # memoize for performance
+      def has_blind_indexes?(klass)
+        if @@blind_index_cache[klass].nil?
+          @@blind_index_cache[klass] = klass.respond_to?(:blind_indexes)
         end
+        @@blind_index_cache[klass]
       end
     end
 
