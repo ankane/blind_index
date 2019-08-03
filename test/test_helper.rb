dc424495b5f15f387f2d76a7b71b31ab5e51d454
Andrew Kane <andrew@chartkick.com>
2019-08-02 21:17:38 -0700

    Updates [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,16 +15,17 @@ else
 end
 
 class User
+  # must come before blind_index
+  before_validation :set_initials, if: -> { changes.key?(:first_name) || changes.key?(:last_name) }
+
   blind_index :email
   blind_index :email_ci, algorithm: :scrypt, attribute: :email, expression: ->(v) { v.try(:downcase) }
-  # blind_index :email_binary, algorithm: :argon2, key: BlindIndex.generate_key, attribute: :email, encode: false
+  blind_index :email_binary, algorithm: :argon2, key: BlindIndex.generate_key, attribute: :email, encode: defined?(Mongoid) # can't get binary working with Mongoid
   blind_index :initials, key: BlindIndex.generate_key, size: 16
   blind_index :phone
 
   validates :email, uniqueness: true
   validates :email_ci, uniqueness: true
-
-  before_validation :set_initials, if: -> { changes.key?(:first_name) || changes.key?(:last_name) }
 
   def set_initials
     self.initials = [first_name.first, last_name.first].join
