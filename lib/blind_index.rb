b29156540013ebaac3a897b853564dbb0c2c5212
Andrew Kane <andrew@ankane.org>
2024-06-15 09:29:06 -0700

    Added todo [skip ci]

--- lib/blind_index.rb
+++ lib/blind_index.rb
@@ -86,6 +86,8 @@ module BlindIndex
           n = cost_options[:n] || 4096
           r = cost_options[:r] || 8
           cp = cost_options[:p] || 1
+          # TODO use
+          # OpenSSL::KDF.scrypt(value, salt: key, N: n, r: r, p: cp, length: size)
           SCrypt::Engine.scrypt(value, key, n, r, cp, size)
         else
           raise BlindIndex::Error, "Unknown algorithm"
